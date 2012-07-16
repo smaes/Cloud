ffc6f95f9e7c5ed93b24b6cf2e08a1d3b0031f4b
smaes <smaes_leo@yahoo.com>
2012-07-16 12:24:45 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,11 @@
+<div style="float:left; width:18%; text-align:right; padding-right:20px;">
+<img src="images/adminq.png" height="130" /></div>
+<div style="float:left; width:40%;">
+<h2>Login</h2>
+<form action="ceklogin.php" method="post">
+<table height="80">
+<tr height="20"><td>Username</td><td>:</td><td><input type="text" name="username" value="Username" onblur="if(this.value=='') this.value='Username';" onfocus="if(this.value=='Username') this.value='';" /></td></tr>
+<tr height="20"><td>Password</td><td>:</td><td><input type="password" name="password" value="Password" onblur="if(this.value=='') this.value='Password';" onfocus="if(this.value=='Password') this.value='';"/></td></tr>
+<tr height="20"><td></td><td></td><td><input type="submit" name="ok" value="Masuk" style="border:solid 1px #999;" /></td></tr>
+</table>
+</form></div>
